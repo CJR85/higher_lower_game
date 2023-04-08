22ae4c43495ce9910e7cf70e6a5a477bf995867b
Calvin Roberts <roberts.calvin85@gmail.com>
2023-04-08 22:40:45 +0100

    Make game repeatable

--- main.py
+++ main.py
@@ -21,36 +21,39 @@ def check_answer(guess, a_followers, b_followers):
 # Display ASCII art
 print(logo)
 score = 0
-
-# Generate game data
-account_a = random.choice(data)
-account_b = random.choice(data)
-if account_a == account_b:
-    account_a = random.choice(data)
-
-print(f"Compare A: {format_data(account_a)}.")
-print(vs)
-print(f"Against B: {format_data(account_b)}.")
-
-# Ask user to enter a guess
-guess = input("Who has more followers? Type 'A' or 'B': ").lower()
-
-# Check if user's guess is correct
-# Get follower count of each account
-a_follower_count = account_a["follower_count"]
-b_follower_count = account_b["follower_count"]
-is_correct = check_answer(guess, a_follower_count, b_follower_count)
-
-# User feedback on their answer
-# Score keeping
-if is_correct:
-   score += 1
-   print(f"You're right! Current score: {score}")
-else:
-   print(f"Sorry, you're wrong. Final score: {score}")
-
+continue_game = True
 
 # Make game repeatable
+while continue_game:
+  # Generate game data
+  account_a = random.choice(data)
+  account_b = random.choice(data)
+  if account_a == account_b:
+      account_a = random.choice(data)
+
+  print(f"Compare A: {format_data(account_a)}.")
+  print(vs)
+  print(f"Against B: {format_data(account_b)}.")
+
+  # Ask user to enter a guess
+  guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+
+  # Check if user's guess is correct
+  # Get follower count of each account
+  a_follower_count = account_a["follower_count"]
+  b_follower_count = account_b["follower_count"]
+  is_correct = check_answer(guess, a_follower_count, b_follower_count)
+
+  # User feedback on their answer
+  # Score keeping
+  if is_correct:
+    score += 1
+    print(f"You're right! Current score: {score}")
+  else:
+    continue_game = False
+    print(f"Sorry, you're wrong. Final score: {score}")
+
+
 
 # Making account at position B become account at position A
 
