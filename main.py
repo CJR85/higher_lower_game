e698a31f6ad9bfb6df060e8c0d095b3610ff6194
Calvin Roberts <roberts.calvin85@gmail.com>
2023-04-08 21:49:07 +0100

    Add comments to break down tasks

---
+++ main.py
@@ -0,0 +1,21 @@
+# Display ASCII art
+
+# Generate game data
+
+# Format data into printable data
+
+# Ask user to enter a guess
+
+# Check if user's guess is correct
+# Get follower count of each account
+# Use if statement to check if user is correct
+
+# User feedback on their answer
+
+# Score keeping
+
+# Make game repeatable
+
+# Making account at position B become account at position A
+
+# Clear screen between rounds
