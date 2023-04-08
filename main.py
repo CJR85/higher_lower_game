f1bc271776f6e4eab9e3d32eaee0dcc4bad781f1
Calvin Roberts <roberts.calvin85@gmail.com>
2023-04-08 22:04:48 +0100

    Format data

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ from art import logo
 from game_data import data
 import random
 
+# Format data into printable data
+def format_data(account):
+  account_name = account["name"]
+  account_descr = account["description"]
+  account_country = account["country"]
+  return (f"{account_name} a {account_descr} from {account_country}")
+
 # Display ASCII art
 print(logo)
 
@@ -11,7 +18,8 @@ account_b = random.choice(data)
 if account_a == account_b:
     account_a = random.choice(data)
 
-# Format data into printable data
+print(f"Compare A: {format_data(account_a)}.")
+print(f"Compare B: {format_data(account_b)}.")
 
 # Ask user to enter a guess
 
